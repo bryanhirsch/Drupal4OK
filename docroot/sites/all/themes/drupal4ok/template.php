838fcce2c6ab954e6bccc0dcb621752b5f423260
Jacob Sanford <jsanford@unb.ca>
2013-05-25 01:29:59 -0300

    Overrides preview behavior for nodetype /housing_need/ as requested in bryanhirsch/Drupal4OK#132

--- docroot/sites/all/themes/drupal4ok/template.php
+++ docroot/sites/all/themes/drupal4ok/template.php
@@ -112,3 +112,33 @@ function drupal4ok_theme($existing, $type, $theme, $path)
       array('type' => 'inline', 'scope' => 'header', 'every_page' => TRUE, 'weight' => 4));
   }
 }
+
+function drupal4ok_node_preview($variables) {
+  $node = $variables['node'];
+  $output = '<div class="preview">';
+  if ($node->type == 'housing_need') {
+    $elements = node_view($node, 'full');
+    $full = drupal_render($elements);
+    $output .= '<h3 class="post-preview" >' . t('Preview of your posting') . '</h3>';
+    $output .= $full;
+  } else {
+    $preview_trimmed_version = FALSE;
+    $elements = node_view(clone $node, 'teaser');
+    $trimmed = drupal_render($elements);
+    $elements = node_view($node, 'full');
+    $full = drupal_render($elements);
+    // Do we need to preview trimmed version of post as well as full version?
+    if ($trimmed != $full) {
+      drupal_set_message(t('The trimmed version of your post shows what your post looks like when promoted to the main page or when exported for syndication.<span class="no-js"> You can insert the delimiter "&lt;!--break--&gt;" (without the quotes) to fine-tune where your post gets split.</span>'));
+      $output .= '<h3>' . t('Preview trimmed version') . '</h3>';
+      $output .= $trimmed;
+      $output .= '<h3>' . t('Preview full version') . '</h3>';
+      $output .= $full;
+    }
+    else {
+      $output .= $full;
+    }
+  }
+  $output .= "</div>\n";
+  return $output;
+}
